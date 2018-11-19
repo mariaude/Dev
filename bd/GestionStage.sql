f8d033459b0a52d1f0e3f2022db70e1c873a44d7
Mathieu Allard <1710991@cmontmorency.ca>
2018-11-19 13:38:41 -0500

    Nouvelle base de données avec des nouveaux utilisateurs de tests. Mots de passe sur TFS

--- bd/GestionStage.sql
+++ bd/GestionStage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 29, 2018 at 06:20 PM
+-- Generation Time: Nov 19, 2018 at 06:37 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -36,9 +36,11 @@ CREATE TABLE IF NOT EXISTS `candidacies` (
 --
 
 INSERT INTO `candidacies` (`internship_id`, `student_id`) VALUES
-(1, 18),
-(1, 19),
-(23, 19);
+(26, 21),
+(27, 21),
+(28, 21),
+(28, 22),
+(29, 22);
 
 -- --------------------------------------------------------
 
@@ -121,9 +123,8 @@ CREATE TABLE IF NOT EXISTS `convocations` (
 --
 
 INSERT INTO `convocations` (`internship_id`, `student_id`, `created`, `modified`) VALUES
-(1, 18, '2018-10-29 18:00:49', '2018-10-29 18:02:47'),
-(1, 19, '2018-10-29 18:10:04', '2018-10-29 18:10:04'),
-(23, 19, '2018-10-29 18:03:44', '2018-10-29 18:03:44');
+(26, 21, '2018-11-14 22:16:33', '2018-11-14 22:16:33'),
+(29, 22, '2018-11-14 22:17:09', '2018-11-14 22:17:09');
 
 -- --------------------------------------------------------
 
@@ -143,15 +144,15 @@ CREATE TABLE IF NOT EXISTS `enterprises` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `additional_informations` text COLLATE utf8_unicode_ci NOT NULL,
   `enterprise_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `enterprises`
 --
 
 INSERT INTO `enterprises` (`id`, `user_id`, `name`, `adress`, `city`, `province`, `postal_code`, `region`, `active`, `additional_informations`, `enterprise_type`) VALUES
-(1, 1, '2020 Inc', '1700 Boulevard des laurentides', 'Laval', 'Québec', 'H7M 2Y', 'rive nord', 1, 'Type d''établissement: Autre', ''),
-(2, 42, 'Test', '123 rue abc', 'Repentigny', 'Quebec', 'j7k828', '1', 1, '1', 'centreReadaptation');
+(4, 49, 'Advanced Progress', '2639 rue Pavot', 'Laval', 'Quebec', 'H8F5D6', 'Laval', 1, 'Clientèle jeune', 'cliniquePrivee'),
+(6, 50, 'Hôpital Soleil', '7193 avenue Papineau', 'Montreal', 'Quebec', 'H52R6G', 'Montreal', 1, 'Hôpital récemment rénové ', 'centreHospitalier');
 
 -- --------------------------------------------------------
 
@@ -169,9 +170,11 @@ CREATE TABLE IF NOT EXISTS `enterprises_client_types` (
 --
 
 INSERT INTO `enterprises_client_types` (`enterprise_id`, `client_type_id`) VALUES
-(2, 17),
-(2, 19),
-(2, 29);
+(4, 18),
+(6, 18),
+(6, 19),
+(4, 22),
+(6, 22);
 
 -- --------------------------------------------------------
 
@@ -183,6 +186,17 @@ CREATE TABLE IF NOT EXISTS `enterprises_missions` (
   `enterprise_id` int(11) NOT NULL,
   `mission_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `enterprises_missions`
+--
+
+INSERT INTO `enterprises_missions` (`enterprise_id`, `mission_id`) VALUES
+(6, 2),
+(6, 3),
+(6, 6),
+(4, 7),
+(4, 11);
 
 -- --------------------------------------------------------
 
@@ -200,17 +214,17 @@ CREATE TABLE IF NOT EXISTS `internships` (
   `work_hours` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `internships`
 --
 
 INSERT INTO `internships` (`id`, `enterprise_id`, `semester`, `start_date`, `end_date`, `available_places`, `work_hours`, `title`, `description`) VALUES
-(1, 1, 'automne 2018', '2018-09-24', '2018-09-24', 200, '60', 'prog', 'programmer'),
-(21, 1, '2', '0000-00-00', '0000-00-00', 0, '', 'Int1', ''),
-(22, 1, '2', '2018-10-02', '2018-10-02', 0, '', 'Int2', ''),
-(23, 1, '2', '2018-10-02', '2018-10-02', 1, '', 'Int3', '');
+(26, 6, 'winter', '2018-01-21', '2018-05-14', 20, '40', 'Aide physiothérapeutre', 'Porter assistance à l''un de nos physiothérapeutre: Suivi des cas, manipulations etc. '),
+(27, 6, 'autumn', '2018-09-24', '2018-12-10', 5, '30', 'Préposé aux machines', 'Assister et expliquer aux patients comment effectuer les exercices sur les machines spécialisées. '),
+(28, 4, 'autumn', '2018-09-26', '2018-12-10', 10, '25', 'Entraineur', 'Entrainer les patients dans la section gym selon les recommandations du physiothérapeutre. '),
+(29, 4, 'winter', '2018-01-21', '2018-05-14', 5, '35', 'Assistant recherche', 'Assister les recherches menés par nos scientifiques.');
 
 -- --------------------------------------------------------
 
@@ -272,15 +286,15 @@ CREATE TABLE IF NOT EXISTS `students` (
   `informations` text COLLATE utf8_unicode_ci NOT NULL,
   `notes` text COLLATE utf8_unicode_ci NOT NULL,
   `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `students`
 --
 
 INSERT INTO `students` (`id`, `user_id`, `admission_number`, `first_name`, `last_name`, `phone_number`, `informations`, `notes`, `active`) VALUES
-(18, 40, '123456789', 'Mathieu', 'Ca marche', '450..47.4.82.', 'Étudiant qui marche!', '1111111111111', 1),
-(19, 43, '123123123', 'Mathieu', 'Allard', '450..45.0.45.', 'Test', '', 0);
+(21, 47, '201671936', 'Adriana', 'Lamontagne', '514.873.2185.', 'Possède de l''expérience: 1 stage de complété en clinique privé', '', 1),
+(22, 48, '201691648', 'Tristan', 'Baire', '514.916.4991.', '', '', 1);
 
 -- --------------------------------------------------------
 
@@ -293,18 +307,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `role` varchar(40) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `role`) VALUES
-(1, 'cynt@bidon.com', '$2y$10$zQ0PIni8iLdkPjP.m8RB6.3w5gRufEMSCkZtszELjCATsiTKSxBJO', 'admin'),
-(40, 'camarche@gmail.com', '$2y$10$.m3ZX2uUtm9jQnNeU6h.o.lhzOgmzYn5DqgcjH3fjlVX9z7izpl2m', 'student'),
-(41, 'CHSLDMTL@hotmail.com', '$2y$10$e0YwrVYr/1FEUoLtkSWcJedCdKUoA9xGAvUg6OBfDvwZMBymhHALW', 'toBeEnterprise'),
-(42, 'clsclaval@gmail.com', '$2y$10$6nrmvT7lB8WD/6Mgr0dLde3lGAayD9Kg2TOIjycA2fokP0BOsujFK', 'enterprise'),
-(43, 'allard.mathieu7@gmail.com', '$2y$10$8S8tpDf/xfqGuhxh.d7gDeE5vFLVsomZO/GYxcLaNHX.ZbjaThi7a', 'student');
+(1, 'a.letsgostage@gmail.com', '$2y$10$WNhHViUMwxqBBdqFIPxoM.F.PJ0ViPhiCY2SW8L7sMf36KbT34ZgW', 'admin'),
+(47, 'etudiant1.letsgostage@gmail.com', '$2y$10$bOMDMMTN2.bXoddIB3vfeet48uw2r3SDRrsVYJBMAKyCqeQkVC6L2', 'student'),
+(48, 'etudiant2.letsgostage@gmail.com', '$2y$10$lEY1n.pSlXQepI4BLDBZge1FzaThoVfRHL0WTsCijFiXUS3zWMUei', 'student'),
+(49, 'entreprise1.letsgostage@gmail.com', '$2y$10$pPVkMXnPRHsA5x6W.HWMiekXtrvnuiBWSDfOqYOY6mQrN1.l23mfO', 'enterprise'),
+(50, 'entreprise2.letsgostage@gmail.com', '$2y$10$pPVkMXnPRHsA5x6W.HWMiekXtrvnuiBWSDfOqYOY6mQrN1.l23mfO', 'enterprise');
 
 --
 -- Indexes for dumped tables
@@ -385,12 +399,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `enterprises`
 --
 ALTER TABLE `enterprises`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `internships`
 --
 ALTER TABLE `internships`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `missions`
 --
@@ -400,12 +414,12 @@ ALTER TABLE `missions`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=52;
 --
 -- Constraints for dumped tables
 --
