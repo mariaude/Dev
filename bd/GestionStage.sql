ddf0c674cca95673c5d24c399ab6fd65cdca239f
Oussama Youcef Bokari <youcef-pino@hotmail.fr>
2018-12-03 13:57:25 -0500

    "Modificiation de la BD"

--- bd/GestionStage.sql
+++ bd/GestionStage.sql
@@ -2,10 +2,10 @@
 -- version 4.4.15.9
 -- https://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 20, 2018 at 04:15 PM
--- Server version: 5.6.37
--- PHP Version: 7.1.8
+-- Client :  localhost
+-- Généré le :  Lun 03 Décembre 2018 à 18:53
+-- Version du serveur :  5.6.37
+-- Version de PHP :  7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `GestionStage`
+-- Base de données :  `GestionStage`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `candidacies`
+-- Structure de la table `candidacies`
 --
 
 CREATE TABLE IF NOT EXISTS `candidacies` (
@@ -32,20 +32,18 @@ CREATE TABLE IF NOT EXISTS `candidacies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
--- Dumping data for table `candidacies`
+-- Contenu de la table `candidacies`
 --
 
 INSERT INTO `candidacies` (`internship_id`, `student_id`) VALUES
-(26, 21),
-(27, 21),
-(28, 21),
-(28, 22),
-(29, 22);
+(1, 18),
+(1, 19),
+(23, 19);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `client_types`
+-- Structure de la table `client_types`
 --
 
 CREATE TABLE IF NOT EXISTS `client_types` (
@@ -54,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `client_types` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `client_types`
+-- Contenu de la table `client_types`
 --
 
 INSERT INTO `client_types` (`id`, `name`) VALUES
@@ -108,7 +106,7 @@ INSERT INTO `client_types` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `convocations`
+-- Structure de la table `convocations`
 --
 
 CREATE TABLE IF NOT EXISTS `convocations` (
@@ -119,17 +117,18 @@ CREATE TABLE IF NOT EXISTS `convocations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
--- Dumping data for table `convocations`
+-- Contenu de la table `convocations`
 --
 
 INSERT INTO `convocations` (`internship_id`, `student_id`, `created`, `modified`) VALUES
-(26, 21, '2018-11-14 22:16:33', '2018-11-14 22:16:33'),
-(29, 22, '2018-11-14 22:17:09', '2018-11-14 22:17:09');
+(1, 18, '2018-10-29 18:00:49', '2018-10-29 18:02:47'),
+(1, 19, '2018-10-29 18:10:04', '2018-10-29 18:10:04'),
+(23, 19, '2018-10-29 18:03:44', '2018-10-29 18:03:44');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `enterprises`
+-- Structure de la table `enterprises`
 --
 
 CREATE TABLE IF NOT EXISTS `enterprises` (
@@ -144,20 +143,20 @@ CREATE TABLE IF NOT EXISTS `enterprises` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `additional_informations` text COLLATE utf8_unicode_ci NOT NULL,
   `enterprise_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `enterprises`
+-- Contenu de la table `enterprises`
 --
 
 INSERT INTO `enterprises` (`id`, `user_id`, `name`, `adress`, `city`, `province`, `postal_code`, `region`, `active`, `additional_informations`, `enterprise_type`) VALUES
-(4, 49, 'Advanced Progress', '2639 rue Pavot', 'Laval', 'Quebec', 'H8F5D6', 'Laval', 1, 'Clientèle jeune', 'cliniquePrivee'),
-(6, 50, 'Hôpital Soleil', '7193 avenue Papineau', 'Montreal', 'Quebec', 'H52R6G', 'Montreal', 0, 'Hôpital récemment rénové ', 'centreHospitalier');
+(1, 1, '2020 Inc', '1700 Boulevard des laurentides', 'Laval', 'Québec', 'H7M 2Y', 'rive nord', 1, 'Type d''établissement: Autre', ''),
+(2, 42, 'Test', '123 rue abc', 'Repentigny', 'Quebec', 'j7k828', '1', 1, '1', 'centreReadaptation');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `enterprises_client_types`
+-- Structure de la table `enterprises_client_types`
 --
 
 CREATE TABLE IF NOT EXISTS `enterprises_client_types` (
@@ -166,20 +165,18 @@ CREATE TABLE IF NOT EXISTS `enterprises_client_types` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `enterprises_client_types`
+-- Contenu de la table `enterprises_client_types`
 --
 
 INSERT INTO `enterprises_client_types` (`enterprise_id`, `client_type_id`) VALUES
-(4, 18),
-(6, 18),
-(6, 19),
-(4, 22),
-(6, 22);
+(2, 17),
+(2, 19),
+(2, 29);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `enterprises_missions`
+-- Structure de la table `enterprises_missions`
 --
 
 CREATE TABLE IF NOT EXISTS `enterprises_missions` (
@@ -187,21 +184,10 @@ CREATE TABLE IF NOT EXISTS `enterprises_missions` (
   `mission_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `enterprises_missions`
---
-
-INSERT INTO `enterprises_missions` (`enterprise_id`, `mission_id`) VALUES
-(6, 2),
-(6, 3),
-(6, 6),
-(4, 7),
-(4, 11);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `files`
+-- Structure de la table `files`
 --
 
 CREATE TABLE IF NOT EXISTS `files` (
@@ -212,12 +198,21 @@ CREATE TABLE IF NOT EXISTS `files` (
   `modified` datetime NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   `student_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `files`
+--
+
+INSERT INTO `files` (`id`, `name`, `path`, `created`, `modified`, `status`, `student_id`) VALUES
+(5, 'BBB', 'BB', '2018-11-28 00:00:00', '2018-11-13 07:59:18', 1, 20),
+(7, 'Carnet de produit.docx', 'Files/', '2018-11-19 22:15:20', '2018-11-19 22:15:20', 1, 20),
+(11, '1-SollicitationMilieux-12-13-H-Courriels.docx', 'Files/', '2018-11-26 20:33:02', '2018-11-26 20:33:02', 1, 20);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `internships`
+-- Structure de la table `internships`
 --
 
 CREATE TABLE IF NOT EXISTS `internships` (
@@ -230,22 +225,22 @@ CREATE TABLE IF NOT EXISTS `internships` (
   `work_hours` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `internships`
+-- Contenu de la table `internships`
 --
 
 INSERT INTO `internships` (`id`, `enterprise_id`, `semester`, `start_date`, `end_date`, `available_places`, `work_hours`, `title`, `description`) VALUES
-(26, 6, 'winter', '2018-01-21', '2018-05-14', 20, '40', 'Aide physiothérapeutre', 'Porter assistance à l''un de nos physiothérapeutre: Suivi des cas, manipulations etc. '),
-(27, 6, 'autumn', '2018-09-24', '2018-12-10', 5, '30', 'Préposé aux machines', 'Assister et expliquer aux patients comment effectuer les exercices sur les machines spécialisées. '),
-(28, 4, 'autumn', '2018-09-26', '2018-12-10', 10, '25', 'Entraineur', 'Entrainer les patients dans la section gym selon les recommandations du physiothérapeutre. '),
-(29, 4, 'winter', '2018-01-21', '2018-05-14', 5, '35', 'Assistant recherche', 'Assister les recherches menés par nos scientifiques.');
+(1, 1, 'automne 2018', '2018-09-24', '2018-09-24', 200, '60', 'prog', 'programmer'),
+(21, 1, '2', '0000-00-00', '0000-00-00', 0, '', 'Int1', ''),
+(22, 1, '2', '2018-10-02', '2018-10-02', 0, '', 'Int2', ''),
+(23, 1, '2', '2018-10-02', '2018-10-02', 1, '', 'Int3', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `missions`
+-- Structure de la table `missions`
 --
 
 CREATE TABLE IF NOT EXISTS `missions` (
@@ -254,7 +249,7 @@ CREATE TABLE IF NOT EXISTS `missions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `missions`
+-- Contenu de la table `missions`
 --
 
 INSERT INTO `missions` (`id`, `name`) VALUES
@@ -289,28 +284,7 @@ INSERT INTO `missions` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_links`
---
-
-CREATE TABLE IF NOT EXISTS `password_links` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `uuid` char(36) COLLATE utf8mb4_bin NOT NULL,
-  `created` date NOT NULL,
-  `used` tinyint(4) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Dumping data for table `password_links`
---
-
-INSERT INTO `password_links` (`id`, `user_id`, `uuid`, `created`, `used`) VALUES
-(1, 52, '485fc381-e790-47a3-9794-1337c0a8fe68', '2018-11-19', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
+-- Structure de la table `students`
 --
 
 CREATE TABLE IF NOT EXISTS `students` (
@@ -322,21 +296,23 @@ CREATE TABLE IF NOT EXISTS `students` (
   `phone_number` char(13) COLLATE utf8_unicode_ci NOT NULL,
   `informations` text COLLATE utf8_unicode_ci NOT NULL,
   `notes` text COLLATE utf8_unicode_ci NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `active` tinyint(1) NOT NULL,
+  `hired` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `students`
+-- Contenu de la table `students`
 --
 
-INSERT INTO `students` (`id`, `user_id`, `admission_number`, `first_name`, `last_name`, `phone_number`, `informations`, `notes`, `active`) VALUES
-(21, 47, '201671936', 'Adriana', 'Lamontagne', '514.873.2185.', 'Possède de l''expérience: 1 stage de complété en clinique privé', '', 1),
-(22, 48, '201691648', 'Tristan', 'Baire', '514.916.4991.', '', '', 1);
+INSERT INTO `students` (`id`, `user_id`, `admission_number`, `first_name`, `last_name`, `phone_number`, `informations`, `notes`, `active`, `hired`) VALUES
+(18, 40, '123456789', 'Mathieu', 'Ca marche', '450...4.7.4..', 'Étudiant qui marche!', '1111111111111', 1, 0),
+(19, 43, '123123123', 'Mathieu', 'Allard', '450..45.0.45.', 'Test', '', 0, 0),
+(20, 44, '99999999', 'Gfdg', 'Nnk', '562..32.5.36.', 'J''ai faim', '', 1, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -344,201 +320,183 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `role` varchar(40) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `role`) VALUES
-(1, 'a.letsgostage@gmail.com', '$2y$10$WNhHViUMwxqBBdqFIPxoM.F.PJ0ViPhiCY2SW8L7sMf36KbT34ZgW', 'admin'),
-(47, 'etudiant1.letsgostage@gmail.com', '$2y$10$bOMDMMTN2.bXoddIB3vfeet48uw2r3SDRrsVYJBMAKyCqeQkVC6L2', 'student'),
-(48, 'etudiant2.letsgostage@gmail.com', '$2y$10$lEY1n.pSlXQepI4BLDBZge1FzaThoVfRHL0WTsCijFiXUS3zWMUei', 'student'),
-(49, 'entreprise1.letsgostage@gmail.com', '$2y$10$pPVkMXnPRHsA5x6W.HWMiekXtrvnuiBWSDfOqYOY6mQrN1.l23mfO', 'enterprise'),
-(50, 'entreprise2.letsgostage@gmail.com', '$2y$10$pPVkMXnPRHsA5x6W.HWMiekXtrvnuiBWSDfOqYOY6mQrN1.l23mfO', 'enterprise'),
-(52, 'resetmeup@gmail.com', '$2y$10$LiU2U6YoSlnjEuV.ejV5hOJ42aetByq2Idnbf.xJsfAjzwedScMMS', 'student');
+(1, 'cynt@bidon.com', '$2y$10$zQ0PIni8iLdkPjP.m8RB6.3w5gRufEMSCkZtszELjCATsiTKSxBJO', 'admin'),
+(40, 'camarche@gmail.com', '$2y$10$.m3ZX2uUtm9jQnNeU6h.o.lhzOgmzYn5DqgcjH3fjlVX9z7izpl2m', 'student'),
+(41, 'CHSLDMTL@hotmail.com', '$2y$10$e0YwrVYr/1FEUoLtkSWcJedCdKUoA9xGAvUg6OBfDvwZMBymhHALW', 'toBeEnterprise'),
+(42, 'clsclaval@gmail.com', '$2y$10$6nrmvT7lB8WD/6Mgr0dLde3lGAayD9Kg2TOIjycA2fokP0BOsujFK', 'enterprise'),
+(43, 'allard.mathieu7@gmail.com', '$2y$10$8S8tpDf/xfqGuhxh.d7gDeE5vFLVsomZO/GYxcLaNHX.ZbjaThi7a', 'student'),
+(44, 'etudiant1.letsgostage@gmail.com', '$2y$10$8hVn1u.pLGY.zRk/dLvsauXZi/wR0CsrLdcMkVgOobygga8Fo/pR6', 'student');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `candidacies`
+-- Index pour la table `candidacies`
 --
 ALTER TABLE `candidacies`
   ADD PRIMARY KEY (`internship_id`,`student_id`),
   ADD KEY `student_key` (`student_id`);
 
 --
--- Indexes for table `client_types`
+-- Index pour la table `client_types`
 --
 ALTER TABLE `client_types`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `convocations`
+-- Index pour la table `convocations`
 --
 ALTER TABLE `convocations`
   ADD PRIMARY KEY (`internship_id`,`student_id`),
   ADD KEY `FK_Candidacies` (`student_id`,`internship_id`);
 
 --
--- Indexes for table `enterprises`
+-- Index pour la table `enterprises`
 --
 ALTER TABLE `enterprises`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `enterprises_client_types`
+-- Index pour la table `enterprises_client_types`
 --
 ALTER TABLE `enterprises_client_types`
   ADD PRIMARY KEY (`enterprise_id`,`client_type_id`),
   ADD KEY `client_type_key` (`client_type_id`);
 
 --
--- Indexes for table `enterprises_missions`
+-- Index pour la table `enterprises_missions`
 --
 ALTER TABLE `enterprises_missions`
   ADD PRIMARY KEY (`enterprise_id`,`mission_id`),
   ADD KEY `mission_key` (`mission_id`);
 
 --
--- Indexes for table `files`
+-- Index pour la table `files`
 --
 ALTER TABLE `files`
   ADD PRIMARY KEY (`id`),
   ADD KEY `student_id` (`student_id`);
 
 --
--- Indexes for table `internships`
+-- Index pour la table `internships`
 --
 ALTER TABLE `internships`
   ADD PRIMARY KEY (`id`),
   ADD KEY `enterprise_id` (`enterprise_id`);
 
 --
--- Indexes for table `missions`
+-- Index pour la table `missions`
 --
 ALTER TABLE `missions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `password_links`
---
-ALTER TABLE `password_links`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_user` (`user_id`);
-
---
--- Indexes for table `students`
+-- Index pour la table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `enterprises`
+-- AUTO_INCREMENT pour la table `enterprises`
 --
 ALTER TABLE `enterprises`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `files`
+-- AUTO_INCREMENT pour la table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
--- AUTO_INCREMENT for table `internships`
+-- AUTO_INCREMENT pour la table `internships`
 --
 ALTER TABLE `internships`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
--- AUTO_INCREMENT for table `missions`
+-- AUTO_INCREMENT pour la table `missions`
 --
 ALTER TABLE `missions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 --
--- AUTO_INCREMENT for table `password_links`
---
-ALTER TABLE `password_links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT pour la table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables exportées
 --
 
 --
--- Constraints for table `candidacies`
+-- Contraintes pour la table `candidacies`
 --
 ALTER TABLE `candidacies`
   ADD CONSTRAINT `candidacies_ibfk_1` FOREIGN KEY (`internship_id`) REFERENCES `internships` (`id`),
   ADD CONSTRAINT `candidacies_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
 
 --
--- Constraints for table `convocations`
+-- Contraintes pour la table `convocations`
 --
 ALTER TABLE `convocations`
   ADD CONSTRAINT `FK_Candidacies` FOREIGN KEY (`student_id`, `internship_id`) REFERENCES `candidacies` (`student_id`, `internship_id`);
 
 --
--- Constraints for table `enterprises`
+-- Contraintes pour la table `enterprises`
 --
 ALTER TABLE `enterprises`
   ADD CONSTRAINT `enterprises_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `enterprises_client_types`
+-- Contraintes pour la table `enterprises_client_types`
 --
 ALTER TABLE `enterprises_client_types`
   ADD CONSTRAINT `enterprises_client_types_ibfk_1` FOREIGN KEY (`client_type_id`) REFERENCES `client_types` (`id`),
   ADD CONSTRAINT `enterprises_client_types_ibfk_2` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprises` (`id`);
 
 --
--- Constraints for table `enterprises_missions`
+-- Contraintes pour la table `enterprises_missions`
 --
 ALTER TABLE `enterprises_missions`
   ADD CONSTRAINT `enterprises_missions_ibfk_1` FOREIGN KEY (`mission_id`) REFERENCES `missions` (`id`),
   ADD CONSTRAINT `enterprises_missions_ibfk_2` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprises` (`id`);
 
 --
--- Constraints for table `files`
+-- Contraintes pour la table `files`
 --
 ALTER TABLE `files`
   ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
 
 --
--- Constraints for table `internships`
+-- Contraintes pour la table `internships`
 --
 ALTER TABLE `internships`
   ADD CONSTRAINT `internships_ibfk_1` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprises` (`id`);
 
 --
--- Constraints for table `password_links`
---
-ALTER TABLE `password_links`
-  ADD CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `students`
+-- Contraintes pour la table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
